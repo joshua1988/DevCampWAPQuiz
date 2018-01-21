f12b6b74682bcf7867be208048f1eb3fb1834426
천정훈 <csdcij@gmail.com>
2018-01-21 14:12:46 +0900

    prototype 과제 제출

---
+++ js-best-practices/cheonjeonghoon/prototype.js
@@ -0,0 +1,29 @@
+function Developer(specialty, language, tool) {
+  this.specialty = specialty;
+  this.language = language;
+  this.tool = tool;
+  this.githubPage = function () {
+    alert("http://username.github.io/");
+  };
+  this.gcp = function () {
+    alert('http://firebase.com/');
+  };
+}
+var me = new Developer("front-end", "javascript", "atom");
+console.log(me);
+
+
+// 퀴즈 - 위 생성자 함수의 githubPage() 와 gcp() 를 Prototype 으로 분리하고,
+//        자신의 분야(specialty), 주력언어(language), 개발툴(tool) 을 갖는 student 객체를 생성해보세요.
+function Developer(specialty, language, tool) {
+  this.specialty = specialty;
+  this.language = language;
+  this.tool = tool;
+}
+
+Developer.prototype = {
+  // Github에 게시해두었습니다..
+  githubPage : function(){alert("https://cheon-jh.github.io/HomeWork/");},
+  gcp :  function(){alert('http://firebase.com/');}
+};
+var student = new Developer('Virtual Machine','C#','VS2015');
